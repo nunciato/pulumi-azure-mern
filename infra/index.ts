4a8966e0339af0518fd3282394205a8339f5a708
Christian Nunciato <c@nunciato.org>
2022-09-02 10:46:11 -0700

    Actually save first

--- infra/index.ts
+++ infra/index.ts
@@ -49,3 +49,97 @@ const connStrings = listConnStrings.connectionStrings;
 export const connString = connStrings?.apply( s => {
     return s?.find( x => x.description === "Primary MongoDB Connection String" )?.connectionString || "";
 });
+
+// Uhhh...
+
+// --- Create the storage and upload backend app as an archive ---
+const storageAccount = new storage.StorageAccount("sa", {
+    resourceGroupName: resourceGroup.name,
+    kind: storage.Kind.StorageV2,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+});
+
+const storageContainer = new storage.BlobContainer("container", {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    publicAccess: storage.PublicAccess.Blob,
+});
+
+const backendBuild = new local.Command("backendBuild", {
+    dir: "../backend",
+    create: pulumi.interpolate `npm install && npm run build`,
+});
+
+const blob = new storage.Blob("blob", {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    containerName: storageContainer.name,
+    source: new pulumi.asset.FileArchive("../backend"),
+});
+
+// /uhh
+
+// --- Make an App Service WebApp from the uploaded storage blob ---
+const backendPlan = new web.AppServicePlan("backendPlan", {
+    kind: "linux",
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    name: "backendService",
+    reserved: true,
+    sku: {
+        tier: "Free",
+        name: "F1",
+        capacity: 1,
+    }
+});
+
+// Note: The name of this WebApp must be globally unique so you should choose your own
+const backend = new web.WebApp("backendPulumiApp", {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    serverFarmId: backendPlan.id,
+    name: "backendPulumiApp",
+    siteConfig: {
+        appSettings: [
+            {
+                name: "DATABASE_URL",
+                value: connString,
+            },
+            {
+                name: "WEBSITE_RUN_FROM_PACKAGE",
+                value: blob.url,
+            },
+        ],
+        cors: {
+            allowedOrigins: [ "*" ],
+        },
+        appCommandLine: "npm start",
+        linuxFxVersion: "NODE|16-lts",
+        use32BitWorkerProcess: true,
+    }
+});
+
+export const backendUrl = pulumi.interpolate `https://${backend.defaultHostName}`;
+
+// --- Create the frontend ---
+const app = new web.StaticSite("frontend", {
+    resourceGroupName: resourceGroup.name,
+    location: "westus2",
+    name: "frontendWebsite",
+    branch: "main",
+    repositoryUrl: config.require( "repo" ),
+    repositoryToken: config.require( "gh_token" ),
+    buildProperties: {
+        appBuildCommand: pulumi.interpolate `export VITE_API_URL=${backendUrl} && npm install && npm run build`,
+        outputLocation: "dist",
+        appLocation: "frontend",
+    },
+    sku: {
+        name: "Free",
+        tier: "Free",
+    },
+});
+
+export const url = app.defaultHostname;
